facbee1b5fd20b0db057d4ae40b3f6e22b98ac1b
qiaoyang <1196317006@qq.com>
2018-10-31 08:25:53 +0800

    css-loader,file-loader,style-loader,url-loader

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './main.css'
-import girl from './girl.jpg'
-import timg from './timg.jpg'
+import girl from './common/girl.jpg'
+import timg from './common/timg.jpg'
+import a from './a.css'
+import b from './b.css'
+console.log(a)
+console.log(b)
 ReactDom.render(
   <div>
-    <h1>hellow world!</h1>
+    <h1 class={a.name}>hellow world!</h1>
+    <div class={b.name}>12312</div>
     <img src={girl} alt=""/>
     <img src={timg} />
   </div>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //webpack 基于node运行环境
 const path=require('path')
 const htmlWebpackPlugin=require('html-webpack-plugin')
+const webpack = require('webpack')
 module.exports={
   mode: 'production',
   entry:'./src/app.js',
@@ -10,13 +11,14 @@ module.exports={
   },
   devServer:{
     open:true,
-    // hot:true
+    hot:true, //热加载
   },
   plugins:[
     new htmlWebpackPlugin({
       // filename:'main.html', //默认是index.html，文件重命名
-      template:path.resolve(__dirname,'dist/template.html'),
-    })
+      template:path.resolve(__dirname,'public/template.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(), //热加载
   ],
   module:{
     // loader处理模块的内容的
@@ -30,9 +32,35 @@ module.exports={
           }
         }]
       },
+      // {
+      //   test:/\.css$/,
+      //   use:['style-loader','css-loader'] //解析顺序重后往前
+      // },
       {
-        test:/\.css/,
-        use:['style-loader','css-loader'] //解析顺序重后往前
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          {
+            loader:'css-loader',
+            options:{
+              modules:true,  //css模块化，默认是fasle,
+              //path:文件夹名，name:文件名，local:定义的class名
+              localIdentName:'[path][name]_[local]_[hash:base64:8]'
+            }
+          }
+        ],
+        exclude:[//排除不需要模块化的文件
+          path.resolve(__dirname,'node_modules'),
+          path.resolve(__dirname,'node_modules'),
+        ] 
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader'],
+        include:[//不需要模块化的文件
+          path.resolve(__dirname,'node_modules'),
+          path.resolve(__dirname,'node_modules'),
+        ]
       },
       // {
       //   test:/\.(jpg|png|gif|jpeg)/,
@@ -43,6 +71,7 @@ module.exports={
         use:[{
           loader:'url-loader',
           options:{
+            //小于base64处理，避免不必要的请求，大于则发起请求并做打包处理
             limit:10000
           }
         }]
